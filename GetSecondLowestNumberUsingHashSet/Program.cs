0e01abc6d7b3d7c3ef8fc49c4f9c8b4b8c58c7e1
raysaik <saikat_ray@outlook.com>
2022-07-11 15:41:27 +0530

    changes for HashSet

---
+++ GetSecondLowestNumberUsingHashSet/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.ReadLine();
+string inputString = "asd3435tyhtrg";
+HashSet<int> hashSetInt = new HashSet<int>();
+foreach(char c in inputString)
+{
+    int num;
+    if(Int32.TryParse(c.ToString(),out num))
+    {
+        hashSetInt.Add(num);
+    }    
+}
+hashSetInt = hashSetInt.OrderByDescending(h => h).ToHashSet<int>();
+Console.WriteLine(hashSetInt.ElementAt(1));
+
